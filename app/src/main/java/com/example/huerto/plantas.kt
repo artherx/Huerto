fd2c56581b33e3e7f985e10065b9630e46747cc3
artherx <xxartherxx@gmail.com>
2023-03-11 21:57:10 -0500

    Feat: Update activity_main.xml Se añade la calse Plantas para la base de datos e enviar la informacion.

--- app/src/main/java/com/example/huerto/plantas.kt
+++ app/src/main/java/com/example/huerto/plantas.kt
@@ -1,4 +1,8 @@
 package com.example.huerto
 
-class Plantas(val tamaño: Float,val tipoHoja: String? = null, val tipoFruto: String? = null, val epocaFloración:String? = null, val colorFlor : String? = null, val tipoSuelo : String, val necesidadesLuz : Float, val climaAdecuado : String, val humedad : Float, val necesidadesEspacio : Float, var cuidadosEspecíficos : String , var solucion : String){
+open class Plantas(val tamaño: Float, val tipoHoja: String? = null, val tipoFruto: String? = null, val epocaFloración: String, val colorFlor: String? = null, val tipoSuelo: String, val necesidadesLuz: Float, val climaAdecuado: String, val humedad: Float, val necesidadesEspacio: Float, var cuidadosEspecíficos: String, var solucion: String) {
+    // ...
+}
+class Frutales(tamaño: Float, tipoFruto: String, tipoSuelo: String, necesidadesLuz: Float, climaAdecuado: String, humedad: Float, necesidadesEspacio: Float, cuidadosEspecíficos: String, solucion: String, epocaFloración: String) : Plantas(tamaño, tipoFruto = tipoFruto, tipoSuelo = tipoSuelo, necesidadesLuz = necesidadesLuz, climaAdecuado = climaAdecuado, humedad = humedad, necesidadesEspacio = necesidadesEspacio, cuidadosEspecíficos = cuidadosEspecíficos, solucion = solucion, epocaFloración = epocaFloración) {
+    // ...
 }
