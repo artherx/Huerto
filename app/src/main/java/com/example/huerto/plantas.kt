d56ad98c1e883ad254254c26e119f5f28e909e45
artherx <xxartherxx@gmail.com>
2023-03-11 21:20:17 -0500

    Feat: Update activity_main.xml Se añade la calse Plantas para la base de datos e enviar la informacion.

---
+++ app/src/main/java/com/example/huerto/plantas.kt
@@ -0,0 +1,4 @@
+package com.example.huerto
+
+class Plantas(val tamaño: Float,val tipoHoja: String? = null, val tipoFruto: String? = null, val epocaFloración:String? = null, val colorFlor : String? = null, val tipoSuelo : String, val necesidadesLuz : Float, val climaAdecuado : String, val humedad : Float, val necesidadesEspacio : Float, var cuidadosEspecíficos : String , var solucion : String){
+}
